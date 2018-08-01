f01a84958a92e49f6daa5728c18bd0abe37ef491
Moraes <renato.nunes.moraes@accenture.com>
2018-08-01 13:31:18 -0300

    Funcionando

--- app.1.js
+++ app.1.js
@@ -35,6 +35,7 @@ var tableName = 'botdata';
 var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 var inMemoryStorage = new builder.MemoryBotStorage();
+var flag = false;
 
 
 // Create your bot with a function to receive messages from the user
@@ -44,10 +45,10 @@ bot.set('storage', tableStorage);
 
 // Recognizer and and Dialog for preview QnAMaker service
 var previewRecognizer = new builder_cognitiveservices.QnAMakerRecognizer({
-    knowledgeBaseId: process.env.QnAKnowledgebaseId,
-    authKey: process.env.QnAAuthKey || process.env.QnASubscriptionKey
-    // knowledgeBaseId: '02ebdb19-f395-4c30-90b4-552b07ec73ca',
-    // authKey: 'EndpointKey 1c21189c-b265-4e8c-8cc2-d2b009c6457e' || 'c383e1bb933a494aadb89dfa66fe0588', // Backward compatibility with QnAMaker (Preview)
+    // knowledgeBaseId: process.env.QnAKnowledgebaseId,
+    // authKey: process.env.QnAAuthKey || process.env.QnASubscriptionKey
+    knowledgeBaseId: '02ebdb19-f395-4c30-90b4-552b07ec73ca',
+    authKey: 'EndpointKey 1c21189c-b265-4e8c-8cc2-d2b009c6457e' || 'c383e1bb933a494aadb89dfa66fe0588', // Backward compatibility with QnAMaker (Preview)
 });
 
 var basicQnAMakerPreviewDialog = new builder_cognitiveservices.QnAMakerDialog({
@@ -61,34 +62,32 @@ bot.dialog('basicQnAMakerPreviewDialog', basicQnAMakerPreviewDialog);
 
 // Recognizer and and Dialog for GA QnAMaker service
 var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
-    knowledgeBaseId: process.env.QnAKnowledgebaseId,
-    authKey: process.env.QnAAuthKey || process.env.QnASubscriptionKey, // Backward compatibility with QnAMaker (Preview)
-    endpointHostName: process.env.QnAEndpointHostName
+    // knowledgeBaseId: process.env.QnAKnowledgebaseId,
+    // authKey: process.env.QnAAuthKey || process.env.QnASubscriptionKey, // Backward compatibility with QnAMaker (Preview)
+    // endpointHostName: process.env.QnAEndpointHostName
 
-    // knowledgeBaseId: '02ebdb19-f395-4c30-90b4-552b07ec73ca',
-    // authKey: 'EndpointKey 1c21189c-b265-4e8c-8cc2-d2b009c6457e' || 'c383e1bb933a494aadb89dfa66fe0588', // Backward compatibility with QnAMaker (Preview)
-    // endpointHostName: 'https://qnafabrica.azurewebsites.net/qnamaker'
+    knowledgeBaseId: '02ebdb19-f395-4c30-90b4-552b07ec73ca',
+    authKey: 'EndpointKey 1c21189c-b265-4e8c-8cc2-d2b009c6457e' || 'c383e1bb933a494aadb89dfa66fe0588', // Backward compatibility with QnAMaker (Preview)
+    endpointHostName: 'https://qnafabrica.azurewebsites.net/qnamaker'
 });
 
 var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
     recognizers: [recognizer],
     defaultMessage: 'Não sei a resposta para isso ainda. Tente de outra maneira.',
     qnaThreshold: 0.3
-}
-);
+});
 
 bot.dialog('basicQnAMakerDialog', basicQnAMakerDialog);
 
 bot.dialog('/', //basicQnAMakerDialog);
     [
         function (session) {
-            var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
-            var qnaAuthKey = process.env.QnAAuthKey || process.env.QnASubscriptionKey;
-            var endpointHostName = process.env.QnAEndpointHostName;
-
-            // var qnaKnowledgebaseId = '02ebdb19-f395-4c30-90b4-552b07ec73ca';
-            // var qnaAuthKey = 'EndpointKey 1c21189c-b265-4e8c-8cc2-d2b009c6457e' || 'c383e1bb933a494aadb89dfa66fe0588';
-            // var endpointHostName = 'https://qnafabrica.azurewebsites.net/qnamaker';
+            // var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
+            // var qnaAuthKey = process.env.QnAAuthKey || process.env.QnASubscriptionKey;
+            // var endpointHostName = process.env.QnAEndpointHostName;
+            var qnaKnowledgebaseId = '02ebdb19-f395-4c30-90b4-552b07ec73ca';
+            var qnaAuthKey = 'EndpointKey 1c21189c-b265-4e8c-8cc2-d2b009c6457e' || 'c383e1bb933a494aadb89dfa66fe0588';
+            var endpointHostName = 'https://qnafabrica.azurewebsites.net/qnamaker';
 
             // QnA Subscription Key and KnowledgeBase Id null verification
             if ((qnaAuthKey == null || qnaAuthKey == '') || (qnaKnowledgebaseId == null || qnaKnowledgebaseId == ''))
@@ -101,5 +100,41 @@ bot.dialog('/', //basicQnAMakerDialog);
                     // Replace with GA QnAMakerDialog service
                     session.replaceDialog('basicQnAMakerDialog');
             }
-        }
-    ]);
+        },
+        function (session) {
+            // Trigger Azure Search dialogs 
+            SearchLibrary.begin(session);
+        },
+        function (session, args) {
+            // Process selected search results
+            session.send(
+                'Search Completed!',
+                args.selection.map( function (i) { return i.key; }).join(', ') ); // format your response 
+        }]);
+
+/* // Azure Search provider
+var azureSearchClient = AzureSearch.create('qnafabrica-asv6uw2veotpxvm', '319E59B619DD5E4A6450B55FFC4F1A45', 'botfabrica');
+var ResultsMapper = SearchLibrary.defaultResultsMapper(ToSearchHit);
+
+// Register Search Dialogs Library with bot
+bot.library(SearchLibrary.create({
+    multipleSelection: true,
+    search: function (query) { return azureSearchClient.search(query).then(ResultsMapper); },
+    refiners: ['Definição', 'Teste'],
+    refineFormatter: function (refiners) {
+        return _.zipObject(
+            refiners.map(function (r) { return 'By ' + _.capitalize(r); }),
+            refiners);
+    }
+}));
+
+// Maps the AzureSearch Job Document into a SearchHit that the Search Library can use
+function ToSearchHit(azureResponse) {
+    return {
+        // define your own parameters 
+        key: azureResponse.id,
+        title: azureResponse.title,
+        description: azureResponse.description,
+        imageUrl: azureResponse.thumbnail
+    };
+} */
